8c1655d8c9d2fb8b80187e2c3acfc4cdd2ee3619
Dasha Lary <dk2800@nyu.edu>
2020-09-23 19:19:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ if valid? && @sender.balance >= @amount && @status == "pending"
   @sender.balance = @sender.balance - @amount 
   @receiver.balance = @receiver.balance + @amount 
   @status = "complete"
-  else
+  elsif valid? == false || @sender.balance < @amount || @status != "pending"
   @status = "rejected"
   puts "Transaction rejected. Please check your account balance."
   end
