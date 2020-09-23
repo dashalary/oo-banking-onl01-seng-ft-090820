c89ddc4902234e8e0adfd8161328f82218e4e74b
Dasha Lary <dk2800@nyu.edu>
2020-09-23 19:15:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ def initialize(sender, receiver, amount)
   end
  
 def execute_transaction
-if valid? && @sender.balance >= @amount && @status == "pending"
+if @sender.valid? && @sender.balance >= @amount && @status == "pending"
   @sender.balance = @sender.balance - @amount 
   @receiver.balance = @receiver.balance + @amount 
   @status = "complete"
