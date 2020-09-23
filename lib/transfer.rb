5a22c3ba93c1ff9ce6e06a21eb1b19b6e39b1657
Dasha Lary <dk2800@nyu.edu>
2020-09-23 19:12:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,7 @@ if valid? && @sender.balance >= @amount && @status == "pending"
   
  
 def reverse_transfer 
-
+if valid? && @receiver.balance >= @amount && @status == "complete"
 @receiver.balance = @receiver.balance - @amount 
 @sender.balance = @sender.balance + @amount
 @status = "reversed"
